e1e4d0a6d0f668b3fd1e89803a4b104f70604b25
Ali <abhatti89@gmail.com>
2022-09-24 20:45:29 +0100

    RSpec & program updates

---
+++ spec/integration_spec.rb
@@ -0,0 +1,37 @@
+require_relative "../lib/diary"
+require_relative "../lib/diary_entry"
+
+RSpec.describe "integration" do
+  it "lists out the entries added" do
+    diary = Diary.new
+    diary_entry_1 = DiaryEntry.new("my title", "my contents")
+    diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+    diary.add(diary_entry_1)
+    diary.add(diary_entry_2)
+    expect(diary.all).to eq [diary_entry_1, diary_entry_2]
+  end
+
+  describe "word counting behaviour" do
+    it "counts the words in all diary entries" do
+      diary = Diary.new
+      diary_entry_1 = DiaryEntry.new("my title", "my contents")
+      diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+      diary.add(diary_entry_1)
+      diary.add(diary_entry_2)
+      expect(diary.count_words).to eq 5
+    end
+  end
+
+  describe "reading time behaviou" do
+    # Remember the case where wpm is 0
+
+    xit "calculates the reading time for all entries" do
+      diary = Diary.new
+      diary_entry_1 = DiaryEntry.new("my title", "my contents")
+      diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+      diary.add(diary_entry_1)
+      diary.add(diary_entry_2)
+      expect(diary.reading_time(2)).to eq 3
+    end
+  end
+end
